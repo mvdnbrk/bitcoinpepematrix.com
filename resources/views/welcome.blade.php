1701b1afe7ac5a8eace824ba032cb0393991a0f4
Mark van den Broek <mvdnbrk@protonmail.com>
2024-04-22 14:25:13 +0200

    Update Twitter link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
 
         <div class="w-full max-w-2xl text-sm mx-auto flex align-middle px-4 sm:px-6 md:px-8 text-gray-400">
             <div class="p-6 grid grid-cols-3 divide-x divide-gray-700 w-full">
-                <a href="https://twitter.com/btc_pepe_matrix"
+                <a href="https://twitter.com/BTCPepeMatrix"
                     class="border-b border-dotted border-gray-500 hover:text-gray-300" target="_blank"
                     rel="noopener">
                         Twitter
