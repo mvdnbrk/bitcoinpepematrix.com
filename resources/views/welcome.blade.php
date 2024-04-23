ff243193f9ac4afd1b1e482dca189942da4007e4
Mark van den Broek <mvdnbrk@protonmail.com>
2024-04-23 14:28:52 +0200

    Fornatting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,15 +39,20 @@
 
             <div class="mt-6 w-full max-w-2xl text-sm mx-auto flex px-4 sm:px-6 md:px-8 text-slate-400">
                 <div class="p-2 flex w-full">
-                    <a href="https://linktr.ee/bitcoinpepematrix"
+                    <a
+                        href="https://linktr.ee/bitcoinpepematrix"
                         class="hover:text-slate-200" target="_blank"
-                        rel="noopener">
+                        rel="noopener"
+                    >
                         <span class="sr-only">Linktree</span>
                         <x-icon-linktree class="w-6 h-6" />
                     </a>
-                    <a href="https://twitter.com/BTCPepeMatrix"
-                        class="pl-6 hover:text-slate-200" target="_blank"
-                        rel="noopener">
+                    <a
+                        href="https://twitter.com/BTCPepeMatrix"
+                        class="pl-6 hover:text-slate-200"
+                        target="_blank"
+                        rel="noopener"
+                    >
                         <span class="sr-only">Twitter / X</span>
                         <x-icon-twitter-x class="w-6 h-6" />
                     </a>
