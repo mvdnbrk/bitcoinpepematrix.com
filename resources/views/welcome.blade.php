e5464e08b7089836df3575b3711d1aa209d4d53d
Mark van den Broek <mvdnbrk@protonmail.com>
2024-04-23 07:57:55 +0200

    Update holder count

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                     </div>
                       <div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                         <dt class="text-sm font-medium leading-6 text-white">HOLDER COUNT</dt>
-                        <dd class="mt-1 text-sm leading-6 text-slate-400 sm:col-span-2 sm:mt-0">~ 16000</dd>
+                        <dd class="mt-1 text-sm leading-6 text-slate-400 sm:col-span-2 sm:mt-0">~ 16500</dd>
                     </div>
                  </dl>
             </div>
