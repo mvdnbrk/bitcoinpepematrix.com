895d8503814d8c199c624cd2d126947f0d530b51
Mark van den Broek <mvdnbrk@protonmail.com>
2024-04-24 19:26:20 +0200

    Extract social links to element

---
+++ resources/views/components/social-links.blade.php
@@ -0,0 +1,26 @@
+<a
+    href="https://linktr.ee/bitcoinpepematrix"
+    class="hover:text-slate-200" target="_blank"
+    rel="noopener"
+>
+    <span class="sr-only">Linktree</span>
+    <x-icon-linktree class="w-6 h-6" />
+</a>
+<a
+    href="https://twitter.com/BTCPepeMatrix"
+    class="pl-6 hover:text-slate-200"
+    target="_blank"
+    rel="noopener"
+>
+    <span class="sr-only">Twitter / X</span>
+    <x-icon-twitter-x class="w-6 h-6" />
+</a>
+<a
+    href="https://t.me/bitcoinpepematrix"
+    class="pl-6 hover:text-slate-200"
+    target="_blank"
+    rel="noopener"
+>
+    <span class="sr-only">Telegram</span>
+    <x-icon-telegram class="w-6 h-6" />
+</a>
