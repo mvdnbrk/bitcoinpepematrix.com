5eeafa05214dc6f6f6321d386dcf94e7356808e6
Mark van den Broek <mvdnbrk@protonmail.com>
2024-04-24 21:13:30 +0200

    Extract marketplaces to component

---
+++ resources/views/components/marketplaces.blade.php
@@ -0,0 +1,24 @@
+<a
+    href="https://magiceden.io/runes/BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX"
+    class="hover:text-slate-300"
+    target="_blank"
+    rel="noopener"
+>
+        trade on magic eden
+</a>
+<a
+    href="https://unisat.io/runes/market?tick=BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX"
+    class="pl-12 hover:text-slate-300"
+    target="_blank"
+    rel="noopener"
+>
+        trade on unisat
+</a>
+<a
+    href="https://www.okx.com/nl/web3/marketplace/runes/token/BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX/840202:2950"
+    class="pl-6 hover:text-slate-300"
+    target="_blank"
+    rel="noopener"
+>
+        trade on okx
+</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,30 +45,7 @@
 
             <div class="mt-4 w-full max-w-2xl text-sm mx-auto flex px-4 sm:px-6 md:px-8 text-white">
                 <div class="p-2 flex w-full">
-                    <a
-                        href="https://magiceden.io/runes/BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX"
-                        class="hover:text-slate-300"
-                        target="_blank"
-                        rel="noopener"
-                    >
-                            trade on magic eden
-                    </a>
-                    <a
-                        href="https://unisat.io/runes/market?tick=BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX"
-                        class="pl-12 hover:text-slate-300"
-                        target="_blank"
-                        rel="noopener"
-                    >
-                            trade on unisat
-                    </a>
-                    <a
-                        href="https://www.okx.com/nl/web3/marketplace/runes/token/BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX/840202:2950"
-                        class="pl-6 hover:text-slate-300"
-                        target="_blank"
-                        rel="noopener"
-                    >
-                            trade on okx
-                    </a>
+                    <x-marketplaces/>
                 </div>
             </div>
 
