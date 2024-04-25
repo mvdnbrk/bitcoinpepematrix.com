89707e32a4a47e1d3a988400aae1594127db0069
Mark van den Broek <mvdnbrk@protonmail.com>
2024-04-25 08:21:23 +0200

    Update rune.php

--- config/rune.php
+++ config/rune.php
@@ -4,17 +4,29 @@ return [
 
     'id' => '840202:2950',
 
-    'ticker' => 'BITCOIN•PEPE•MATRIX',
+    'number' => 4241
+
+    'etching_block' => 840202,
+
+    'etching_transaction' => 2950,
+
+    'timestamp' => '2024-04-21 11:46:29 UTC',
 
     'deploy_date' => 'Apr 21 01:46 pm',
 
+    'ticker' => 'BITCOIN•PEPE•MATRIX',
+
     'mint_block_window' => '840269 - 841420',
+
+    'symbol' => 'ℙ',
 
     'decimal' => 0,
 
     'premine' => 0,
 
     'total_supply' => '6.9 B',
+
+    'burned' => 69420,
 
     'approx_holder_count' => 16600,
 
