9ba970d16703c1a4fd03cac7118500fb22ede448
Mark van den Broek <mvdnbrk@protonmail.com>
2024-04-26 06:43:37 +0200

    Update twitter-card.blade.php

--- resources/views/components/twitter-card.blade.php
+++ resources/views/components/twitter-card.blade.php
@@ -1,4 +1,5 @@
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="{{ config('app.name') }}">
+<meta name="twitter:title" content="{{ config('rune.ticker') }}">
 <meta name="twitter:site" content="@BTCPepeMatrix">
 <meta name="twitter:image" content="{{ secure_url('images/bitcoin-pepe-matrix.gif') }}">
+<meta name="twitter:image:alt" content="Pepe on Bitcoin Runes">
