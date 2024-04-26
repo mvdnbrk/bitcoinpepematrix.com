e1ad1b94f2073ce165cf1ddcdd35cebfbbd20731
Mark van den Broek <mvdnbrk@protonmail.com>
2024-04-26 17:46:33 +0200

    Add email link

---
+++ resources/views/components/email-link.blade.php
@@ -0,0 +1,8 @@
+<a
+    href="mailto:{{ config('mail.from.address') }}"
+    class="pl-6 hover:text-slate-200"
+    rel="noopener"
+>
+    <span class="sr-only">E-mail</span>
+    <x-icon-email class="w-6 h-6" />
+</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,6 +56,7 @@
             <div class="border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
                 <div class="flex space-x-6 md:order-2 text-gray-400">
                     <x-social-links/>
+                    <x-email-link/>
                 </div>
                 <p class="flex mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
                     &copy; 2024 {{ config('rune.ticker') }}. All rights reserved.
