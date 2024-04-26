e7b3e3e40978f3a2c434bcf337475641ff422e35
Mark van den Broek <mvdnbrk@protonmail.com>
2024-04-26 20:35:11 +0200

    Adjust padding for email link

--- resources/views/components/email-link.blade.php
+++ resources/views/components/email-link.blade.php
@@ -1,6 +1,6 @@
 <a
     href="mailto:{{ config('mail.from.address') }}"
-    class="pl-6 hover:text-slate-200"
+    class="pl-6 pr-4 hover:text-slate-200"
     rel="noopener"
 >
     <span class="sr-only">E-mail</span>
