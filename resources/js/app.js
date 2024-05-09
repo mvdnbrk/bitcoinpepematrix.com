d3d91ebed09dc00a58fa91cbb5e09638b1dc50d0
Mark van den Broek <mvdnbrk@protonmail.com>
2024-05-09 09:44:09 +0200

    Formatting

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,35 +15,34 @@ const drops = []; // an array of drops - one per column
 
 // Fill the drops[] array with drops from the top of the screen
 for (let x = 0; x < columns; x++) {
-drops[x] = 1;
+    drops[x] = 1;
 }
 
 // Main animation loop
 function draw() {
-// Clear the canvas
-ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-ctx.fillRect(0, 0, matrix.width, matrix.height);
+    // Clear the canvas
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillRect(0, 0, matrix.width, matrix.height);
 
-ctx.fillStyle = "#0F0"; // Green text
-ctx.font = font_size + "px arial";
+    ctx.fillStyle = "#0F0"; // Green text
+    ctx.font = font_size + "px arial";
 
-// Loop through the drops[] array and draw a character for each drop
-for (let i = 0; i < drops.length; i++) {
-let text = characters[Math.floor(Math.random() * characters.length)];
-ctx.fillText(text, i * font_size, drops[i] * font_size);
+    // Loop through the drops[] array and draw a character for each drop
+    for (let i = 0; i < drops.length; i++) {
+        let text = characters[Math.floor(Math.random() * characters.length)];
+        ctx.fillText(text, i * font_size, drops[i] * font_size);
 
-// Reset the drop to the top when it reaches the bottom of the screen
-if (drops[i] * font_size > matrix.height && Math.random() > 0.975) {
-  drops[i] = 0;
-}
+        // Reset the drop to the top when it reaches the bottom of the screen
+        if (drops[i] * font_size > matrix.height && Math.random() > 0.975) {
+            drops[i] = 0;
+        }
 
-// Increment the y position of each drop
-drops[i]++;
+        // Increment the y position of each drop
+        drops[i]++;
+    }
 
-}
-
-// Call the draw() function again in a few milliseconds
-setTimeout(draw, 33);
+    // Call the draw() function again in a few milliseconds
+    setTimeout(draw, 33);
 }
 
 draw();
