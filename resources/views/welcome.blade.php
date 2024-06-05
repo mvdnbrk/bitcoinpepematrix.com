6fc416e7af422cc0c112022d02217f129a3e971a
Mark van den Broek <mvdnbrk@protonmail.com>
2024-06-05 19:31:46 +0200

    Wider  letter spacing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
             <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
                 <x-icon-bitcoin class="h-11 fill-red-600" />
-                <h1 class="mt-10 text-2xl font-bold tracking-tight text-white sm:text-4xl">
+                <h1 class="mt-10 text-2xl font-bold tracking-wider text-white sm:text-4xl">
                     {{ config('rune.ticker') }}
                 </h1>
                 <div class="mt-10 flex items-center gap-x-6 text-slate-300">
