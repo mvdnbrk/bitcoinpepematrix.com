5ebcfc5345ccc8955fac44ca3142085c6f6e751c
Mark van den Broek <mvdnbrk@protonmail.com>
2024-06-19 17:17:56 +0200

    Generate twitter handle from config value

--- resources/views/components/head/meta-twitter.blade.php
+++ resources/views/components/head/meta-twitter.blade.php
@@ -1,5 +1,5 @@
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="{{ config('rune.ticker') }}">
-<meta name="twitter:site" content="@BTCPepeMatrix">
+<meta name="twitter:site" content="{{ Str::of(config('socials.twitter'))->after('https://x.com/')->prepend('@') }}">
 <meta name="twitter:image" content="{{ asset('images/twitter-card-large.png') }}">
 <meta name="twitter:image:alt" content="Pepe on Bitcoin Runes">
