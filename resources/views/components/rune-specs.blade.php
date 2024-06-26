b6ed4a3133d89467f75da1c3168a75f86ab4a672
Mark van den Broek <mvdnbrk@protonmail.com>
2024-06-26 20:06:23 +0200

    Change holder count to holders

--- resources/views/components/rune-specs.blade.php
+++ resources/views/components/rune-specs.blade.php
@@ -88,7 +88,7 @@
     </div>
     <div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
         <dt class="text-sm font-medium leading-6 text-white">
-            HOLDER COUNT
+            HOLDERS
         </dt>
         <dd class="mt-1 text-sm leading-6 text-slate-400 sm:col-span-2 sm:mt-0">
             ~ {{ config('rune.approx_holder_count') }}
