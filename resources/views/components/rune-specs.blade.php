68ae9eb1895641927845cfe471af2f9020fc62ae
Mark van den Broek <mvdnbrk@protonmail.com>
2024-06-26 14:08:42 +0200

    Add rune number

--- resources/views/components/rune-specs.blade.php
+++ resources/views/components/rune-specs.blade.php
@@ -9,6 +9,14 @@
     </div>
     <div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
         <dt class="text-sm font-medium leading-6 text-white">
+            RUNE NUMBER
+        </dt>
+        <dd class="mt-1 text-sm leading-6 text-slate-400 sm:col-span-2 sm:mt-0">
+            {{ config('rune.number') }}
+        </dd>
+    </div>
+    <div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt class="text-sm font-medium leading-6 text-white">
             DEPLOY DATE
         </dt>
         <dd class="mt-1 text-sm leading-6 text-slate-400 sm:col-span-2 sm:mt-0">
