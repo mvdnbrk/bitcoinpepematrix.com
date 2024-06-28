ba8eef905e78147ba8916e62f0cc35c7558d210a
Mark van den Broek <mvdnbrk@protonmail.com>
2024-06-28 13:30:14 +0200

    Add command to cache the holder count

---
+++ app/Console/Commands/CacheHolderCountCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Rune;
+use App\Jobs\CacheHolderCount;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class CacheHolderCountCommand extends Command
+{
+    protected $signature = 'rune:cache-holder-count';
+
+    protected $description = 'Cache the holder count';
+
+    public function handle()
+    {
+        $rune =  new Rune(config('rune'));
+
+        CacheHolderCount::dispatch($rune);
+
+        $this->info('The holder count for '.$rune->ticker.' is '.Cache::get('holder_count', default: 'unknown'));
+    }
+}
