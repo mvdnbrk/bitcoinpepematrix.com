006864f7eb5263bb126e227ba84c56ea92f4ac43
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-01 10:29:08 +0200

    Add SerializesModels

--- app/Jobs/CollectRunePrice.php
+++ app/Jobs/CollectRunePrice.php
@@ -6,13 +6,14 @@ use App\Rune;
 use App\Models\RunePrice;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
 class CollectRunePrice implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected string $apiUrl;
 
