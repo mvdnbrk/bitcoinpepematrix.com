55dde1c4dfd900b063a270e589bd60f94d20613e
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-01 18:51:37 +0200

    Use `$rune->ticker`

--- resources/views/components/head/meta-opengraph.blade.php
+++ resources/views/components/head/meta-opengraph.blade.php
@@ -1,5 +1,5 @@
 <meta property="og:url" content="{{ url()->full() }}">
 <meta property="og:type" content="website">
-<meta property="og:title" content="{{ config('rune.ticker') }}">
+<meta property="og:title" content="{{ $rune->ticker }}">
 <meta property="og:description" content="Pepe on Bitcoin Runes | The ticker is BITCOIN•PEPE•MATRIX  | Community Run">
 <meta property="og:image" content="{{ asset('images/twitter-card-large.png') }}">
