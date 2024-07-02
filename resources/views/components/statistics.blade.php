0f8193159455b371f4e8c3d9609b7863542d4c58
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-02 19:09:47 +0200

    Format the floor price

--- resources/views/components/statistics.blade.php
+++ resources/views/components/statistics.blade.php
@@ -20,7 +20,7 @@
     <div class="flex flex-col gap-y-3 border-l border-white/10 pl-6">
         <dt class="text-sm leading-6">FLOOR PRICE</dt>
         <dd class="order-first text-3xl font-semibold tracking-tight">
-            {{ Cache::get('floor_price') }} sats
+            {{ Number::format(Cache::get('floor_price'), precision: 2) }} sats
         </dd>
     </div>
 </dl>
