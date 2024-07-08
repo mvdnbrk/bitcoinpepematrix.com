0f4d48a41eb16dcd2e74ba5f59b48194744bc0cb
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-08 20:40:08 +0200

    Remove `nl` from okx url

--- config/marketplaces.php
+++ config/marketplaces.php
@@ -6,7 +6,7 @@ return [
 
     'magiceden' => 'https://magiceden.io/runes/BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX',
 
-    'okx' => 'https://www.okx.com/nl/web3/marketplace/runes/token/BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX/840202:2950',
+    'okx' => 'https://www.okx.com/web3/marketplace/runes/token/BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX/840202:2950',
 
     'ordinalswallet' => 'https://ordinalswallet.com/collection/rune-BITCOIN%E2%80%A2PEPE%E2%80%A2MATRIX',
 
