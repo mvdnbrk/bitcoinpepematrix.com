7c340bb9faa193ca3800851a2aaa7e9d6acdf0fd
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-08 14:10:55 +0200

    Add tradingview logo and link

--- resources/views/components/charts.blade.php
+++ resources/views/components/charts.blade.php
@@ -1,3 +1,14 @@
 <div id="trading-view-charts" class="w-full h-96">
 
 </div>
+<div class="w-full flex mt-4 pr-4 lg:pr-12 justify-end text-gray-600">
+    <span class="sr-only">Trading View Charts</span>
+    <a
+        href="https://www.tradingview.com"
+        target="_blank"
+        rel="noopener"
+        class="hover:text-gray-200"
+    >
+        <x-icon-tradingview class="h-4 lg:h-6" />
+    </a>
+</div>
