b30766801294a0879b879f4649ef7c61fb5b3a2c
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-08 19:43:47 +0200

    Adjust font size

--- resources/views/components/marketplaces.blade.php
+++ resources/views/components/marketplaces.blade.php
@@ -1,6 +1,6 @@
 <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl lg:max-w-none">
-        <h2 class="sm:text-xl text-base font-semibold leading-8 text-slate-400">
+        <h2 class="text-lg md:text-xl text-base font-semibold leading-8 text-slate-400">
             marketplaces to trade your most favorite rune
         </h2>
         <div class="mx-auto mt-6 grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-4 text-slate-200">
