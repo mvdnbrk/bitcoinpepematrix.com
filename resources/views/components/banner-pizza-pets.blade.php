b6f36912889d6970b7996f038ada93a3347b6b4f
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-10 19:18:54 +0200

    Change text

--- resources/views/components/banner-pizza-pets.blade.php
+++ resources/views/components/banner-pizza-pets.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ route('pizza_pets') }}">
             <strong class="font-semibold">ANNOUNCEMENT</strong>
             <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>
-            We are thrilled to anncounce a new partnership
+            We are thrilled to anncounce a new collaboration
             <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>
             Read more here
             <span aria-hidden="true">&rarr;</span>
