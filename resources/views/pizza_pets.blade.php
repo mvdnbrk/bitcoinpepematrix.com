b59a66a51a49ea6a010977475acfd3499574ee9d
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-14 09:46:39 +0200

    Adjust layout

--- resources/views/pizza_pets.blade.php
+++ resources/views/pizza_pets.blade.php
@@ -2,11 +2,11 @@
     <div class="relative isolate overflow-hidden">
         <canvas id="matrix" class="absolute -z-10 h-full w-full" aria-hidden="true"></canvas>
         <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
-            <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-                <h1 class="mt-10 p-4 text-2xl font-bold tracking-wider text-white sm:text-4xl">
+            <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl">
+                <h1 class="mt-2 px-4 text-2xl font-bold tracking-wider text-white sm:text-4xl">
                     HAVE AN EGGCELENT DAY!
                 </h1>
-                <div class="mt-6 text-xl text-gray-100 bg-slate-950/75 p-4 rounded-xl">
+                <div class="mt-4 text-xl text-gray-100 bg-slate-950/75 p-4 rounded-xl">
                     <p>
                         We are thrilled to announce a collaboration with
                     </p>
@@ -21,21 +21,21 @@
                     </p>
                 </div>
             </div>
-            <div class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+            <div class="mx-auto flex justify-end">
                 <div class="flex space-x-4 rounded-2xl bg-slate-900 p-2">
                     <img
                         src="{{ asset('images/bitcoin-pepe-matrix.gif') }}"
                         alt="{{ $rune->ticker }}"
                         width="240"
                         height="240"
-                        class="w-[16rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
+                        class="w-60 rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
                     >
                     <img
                         src="{{ asset('https://pbs.twimg.com/tweet_video_thumb/GRlvQgbWEAATtMz.jpg') }}"
                         alt="This is EggCelent!"
                         width="240"
                         height="240"
-                        class="w-[16rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
+                        class="w-60 rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
                     >
                 </div>
             </div>
