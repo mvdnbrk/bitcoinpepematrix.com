6f85f757a68d265c09a201b58f865c2d67a53351
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-15 13:55:35 +0200

    Rename class

--- app/Jobs/VerifyOwnership.php
+++ app/Jobs/VerifyOwnership.php
@@ -11,7 +11,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class VerifyOwnership implements ShouldQueue
+class VerifyRuneOwnership implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
