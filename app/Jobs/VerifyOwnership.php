06842e4fd3acf621d78d979da88969f086f5b72f
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-15 13:49:49 +0200

    Add a job to verify ownership

---
+++ app/Jobs/VerifyOwnership.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Rune;
+use Illuminate\Support\Str;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class VerifyOwnership implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public string $apiUrl;
+
+    public function __construct(
+        public Rune $rune,
+        public string $address,
+    ) {
+        $this->apiUrl = Str::of('https://api-mainnet.magiceden.dev/v2/ord/btc/runes/wallet/balances/')
+            ->append($address)
+            ->append('/')
+            ->append($this->rune->tickerWithoutSpacers);
+    }
+
+    public function handle(): void
+    {
+        $response = Http::withToken(
+            config('services.magiceden.api_key')
+        )
+            ->acceptJson()
+            ->get($this->apiUrl);
+
+        if ($response->successful()) {
+            //
+        }
+    }
+}
