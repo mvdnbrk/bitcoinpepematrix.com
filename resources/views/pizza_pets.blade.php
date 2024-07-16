369844be97eaab23b1b21d24b10c7b620cf6670b
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-16 14:52:29 +0200

    Fix the matrix background on pizza pets page

--- resources/views/pizza_pets.blade.php
+++ resources/views/pizza_pets.blade.php
@@ -3,7 +3,7 @@
     description="We are thrilled to announce a collaboration with Pizza Ninjas and Mega Punks on their upcoming Pizza Pets airdrop."
 >
     <div class="relative isolate overflow-hidden">
-        <canvas id="matrix" class="absolute -z-10 h-full w-full" aria-hidden="true"></canvas>
+        <x-matrix-background/>
         <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
             <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl">
                 <h1 class="mt-2 px-4 text-2xl font-bold tracking-wider text-white sm:text-4xl">
