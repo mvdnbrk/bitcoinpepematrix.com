9f1c126cf046d9d0dc77619aa84f4ff4ce438642
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-16 12:52:51 +0200

    wip

--- resources/views/components/donation.blade.php
+++ resources/views/components/donation.blade.php
@@ -1,10 +1,21 @@
-<div>
-    <label for="donate" class="block text-sm font-medium leading-6 text-white">Donation Wallet Address</label>
-    <div class="w-64 relative mt-2 rounded-md shadow-sm">
-        <input onClick="this.select();" type="text" name="donate" id="donate" class="block w-full rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-green-300 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" value="bc1pwe9y8d4cs5ded84avk7yecaddjm85uvdd45kwmq3etdc29gpcq9qzg588c">
+<div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <label for="donate" class="block text-lg md:text-xl text-base font-semibold leading-8 text-slate-400">
+        donation wallet address
+    </label>
+    <div class="w-96 relative mt-2 rounded-md shadow-sm">
+        <input
+            onClick="this.select();"
+            type="text"
+            name="donate"
+            id="donate"
+            class="block w-full rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-green-300 focus:ring-2 focus:ring-inset focus:ring-green-900 sm:text-sm sm:leading-6"
+            value="bc1pwe9y8d4cs5ded84avk7yecaddjm85uvdd45kwmq3etdc29gpcq9qzg588c"
+        >
         <div class="cursor-pointer absolute inset-y-0 right-0 flex items-center pr-3">
             <copy-button target-element="#donate">
-                <x-icon-copy class="w-5 h-5" />
+                <button class="active:text-green-600">
+                    <x-icon-copy class="w-5 h-5"/>
+                </button>
             </copy-button>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,6 @@
 
 <x-layout.main>
     <x-banner-pizza-pets/>
-
-    <x-donation/>
-
     <div class="relative isolate overflow-hidden">
         <canvas id="matrix" class="absolute -z-10 h-full w-full" aria-hidden="true"></canvas>
         <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
@@ -44,6 +41,10 @@
 
     <x-layout.section>
         <x-statistics/>
+    </x-layout.section>
+
+    <x-layout.section>
+        <x-donation/>
     </x-layout.section>
 
     <x-layout.section>
