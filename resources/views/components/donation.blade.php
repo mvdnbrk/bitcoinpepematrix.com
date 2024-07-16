bc6e7181f84c7e419845a4f76b023d141497aa34
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-16 13:10:57 +0200

    Add `Ordinals` add on to input field

--- resources/views/components/donation.blade.php
+++ resources/views/components/donation.blade.php
@@ -2,13 +2,16 @@
     <label for="donate" class="block text-lg md:text-xl text-base font-semibold leading-8 text-slate-400">
         we are community driven, please consider making a donation to our community wallet
     </label>
-    <div class="w-full relative mt-2 rounded-md shadow-sm">
+    <div class="flex w-full relative mt-2 rounded-md shadow-sm">
+        <span class="bg-white inline-flex items-center rounded-l-md border border-r-0 border-gray-300 px-3 text-slate-500 sm:text-sm">
+            Ordinals
+        </span>
         <input
             onClick="this.select();"
             type="text"
             name="donate"
             id="donate"
-            class="block w-full rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-green-300 focus:ring-2 focus:ring-inset focus:ring-green-900 sm:text-sm sm:leading-6"
+            class="bg-white block w-full min-w-0 flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
             value="bc1pwe9y8d4cs5ded84avk7yecaddjm85uvdd45kwmq3etdc29gpcq9qzg588c"
         >
         <div class="cursor-pointer absolute inset-y-0 right-0 flex items-center pr-3">
