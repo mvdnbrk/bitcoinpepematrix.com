6a695c2edbbd3522ad9e544780456db09503768e
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-16 12:40:07 +0200

    wip

---
+++ resources/views/components/donation.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <label for="donate" class="block text-sm font-medium leading-6 text-white">Donation Wallet Address</label>
+    <div class="w-64 relative mt-2 rounded-md shadow-sm">
+        <input onClick="this.select();" type="text" name="donate" id="donate" class="block w-full rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-green-300 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" value="bc1pwe9y8d4cs5ded84avk7yecaddjm85uvdd45kwmq3etdc29gpcq9qzg588c">
+        <div class="cursor-pointer absolute inset-y-0 right-0 flex items-center pr-3">
+            <copy-button target-element="#donate">
+                <x-icon-copy class="w-5 h-5" />
+            </copy-button>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,17 +2,7 @@
 <x-layout.main>
     <x-banner-pizza-pets/>
 
-<div>
-    <label for="donate" class="block text-sm font-medium leading-6 text-gray-900">Donation Wallet Address</label>
-    <div class="w-64 relative mt-2 rounded-md shadow-sm">
-        <input type="text" name="donate" id="donate" class="block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6" value="bc1pwe9y8d4cs5ded84avk7yecaddjm85uvdd45kwmq3etdc29gpcq9qzg588c">
-        <div class="cursor-pointer absolute inset-y-0 right-0 flex items-center pr-3">
-            <copy-button target-element="#donate">
-                <x-icon-copy class="w-5 h-5" />
-            </copy-button>
-        </div>
-    </div>
-</div>
+    <x-donation/>
 
     <div class="relative isolate overflow-hidden">
         <canvas id="matrix" class="absolute -z-10 h-full w-full" aria-hidden="true"></canvas>
