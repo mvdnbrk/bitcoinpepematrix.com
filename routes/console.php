7bd7e10e935eed7beceae343d7f6df95d295f891
Mark van den Broek <mvdnbrk@protonmail.com>
2024-07-30 17:00:17 +0200

    Update Bitcoin price update frequency

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use App\Jobs\CollectRunePrice;
 use App\Rune;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('bitcoin:cache-price')->everyFiveMinutes();
+Schedule::command('bitcoin:cache-price')->everyTenMinutes();
 Schedule::command('rune:cache-floor-price')->everyFiveMinutes();
 Schedule::command('rune:cache-market-cap')->everyFifteenMinutes();
 Schedule::command('rune:cache-holder-count')->hourly();
